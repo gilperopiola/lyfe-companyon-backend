1c7588ca040414356dc0c4015c17311920bd84a6
Franco Ferraguti <franco.ferraguti@pjn.gov.ar>
2019-11-06 14:38:37 -0300

    improved mails

--- cron.go
+++ cron.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"time"
 
 	"github.com/jasonlvhit/gocron"
-	"github.com/mailjet/mailjet-apiv3-go"
 )
 
 func initCron() {
-	sendDailyMail()
-
 	//you have to take out 3 hours to get the real Argentina time
 	gocron.Every(1).Day().At("10:00").Do(sendDailyMail)
 	gocron.Every(1).Sunday().At("13:00").Do(sendWeeklyDoneMail)
@@ -19,13 +14,19 @@ func initCron() {
 	<-gocron.Start()
 }
 
-func sendDailyMail() {
-	//todo config
-	mailjetClient := mailjet.NewMailjetClient("82a01557701a0dd9f319df7c84418785", "151f081f1debe5f691ac6b0cc6caa8eb")
+func createMailRow(text string, backgroundColor string, foregroundColor string, title bool) string {
+	titleCSS := ""
+	if title != "" {
+		titleCSS = " font-weight: bold; font-size: 16px; text-align: center;"
+	}
 
+	return `<p style="background-color: ` + backgroundColor + ` color: ` + foregroundColor + `; padding: 8px; margin: 0;` + titleCSS + `">` + text + `</p>`
+}
+
+func sendDailyMail() {
+	//get dailies
 	task := &Task{}
 	params := &SearchParameters{
-		Filter:           "",
 		FilterTagID:      1,
 		FilterImportance: 1,
 		ShowPrivate:      true,
@@ -35,8 +36,8 @@ func sendDailyMail() {
 
 	dailies, _ := task.Search(params)
 
+	//get doing
 	params.FilterTagID = 0
-
 	allTasks, _ := task.Search(params)
 	doing := []*Task{}
 	for _, task := range allTasks {
@@ -45,64 +46,42 @@ func sendDailyMail() {
 		}
 	}
 
+	//get done and added
 	doneYesterday, _ := task.GetDoneAndArchivedSince(time.Now().AddDate(0, 0, -1))
 	addedYesterday, _ := task.GetAddedSince(time.Now().AddDate(0, 0, -1))
 
+	//prepare elements
 	dailyElements := ""
-	for i, daily := range dailies {
-		color := "#e4e4e4"
-		if i%2 == 0 {
-			color = "#c3c3c3"
-		}
-
-		dailyElements += `<p style="background-color: ` + color + `; padding: 8px; margin: 0;">` + daily.Name + `</p>`
+	for _, daily := range dailies {
+		dailyElements += createMailRow(daily.Name, "gray", "black", false)
 	}
 
 	doingElements := ""
-	for i, taskDoing := range doing {
-		color := "#e4e4e4"
-		if i%2 == 0 {
-			color = "#c3c3c3"
-		}
-
-		doingElements += `<p style="background-color: ` + color + `; padding: 8px; margin: 0;">` + taskDoing.Name + `</p>`
+	for _, taskDoing := range doing {
+		doingElements += createMailRow(taskDoing.Name, "gray", "black", false)
 	}
 
 	doneYesterdayElements := ""
-	for i, taskDone := range doneYesterday {
-		color := "#e4e4e4"
-		if i%2 == 0 {
-			color = "#c3c3c3"
-		}
-
-		doneYesterdayElements += `<p style="background-color: ` + color + `; padding: 8px; margin: 0;">` + taskDone.Name + `</p>`
+	for _, taskDone := range doneYesterday {
+		doneYesterdayElements += createMailRow(taskDone.Name, "gray", "black", false)
 	}
 
 	addedYesterdayElements := ""
-	for i, taskAdded := range addedYesterday {
-		color := "#e4e4e4"
-		if i%2 == 0 {
-			color = "#c3c3c3"
-		}
-
-		addedYesterdayElements += `<p style="background-color: ` + color + `; padding: 8px; margin: 0;">` + taskAdded.Name + `</p>`
+	for _, taskAdded := range addedYesterday {
+		addedYesterdayElements += createMailRow(taskAdded.Name, "gray", "black", false)
 	}
+
+	//send mail
+	subject := "Daily - " + time.Now().Weekday() + " " + time.Now().Format("06/01/02")
 
 	html := `
 	<html>
-		<head>
-	  		<title>Daily - ` + time.Now().Format("06/01/02") + `</title>
-		</head>
+		<body> ` +
 
-		<body> 
-			<p style="color: white; background-color: black; margin: 0; font-size: 14px; text-align: center; font-weight: bold;">DAILY</p>` +
-		dailyElements + `
-			<p style="color: white; background-color: black; margin: 0; font-size: 14px; text-align: center; font-weight: bold;">DOING</p>` +
-		doingElements + `
-			<p style="color: white; background-color: black; margin: 0; font-size: 14px; text-align: center; font-weight: bold;">DONE / ARCHIVED YESTERDAY</p>` +
-		doneYesterdayElements + `
-			<p style="color: white; background-color: black; margin: 0; font-size: 14px; text-align: center; font-weight: bold;">ADDED YESTERDAY</p>` +
-		addedYesterdayElements + `
+		createMailRow("DAILY", "black", "white", true) + dailyElements +
+		createMailRow("DOING", "black", "white", true) + doingElements +
+		createMailRow("DONE / ARCHIVED YESTERDAY", "black", "white", true) + doneYesterdayElements +
+		createMailRow("ADDED YESTERDAY", "black", "white", true) + addedYesterdayElements + `
 
 			<p style="background-color: black; margin: 0; font-size: 8px">~</p>
 			<br>
@@ -110,38 +89,10 @@ func sendDailyMail() {
 	</html>
 	`
 
-	messagesInfo := []mailjet.InfoMessagesV31{
-		mailjet.InfoMessagesV31{
-			From: &mailjet.RecipientV31{
-				Email: "lucfran2005@hotmail.com",
-				Name:  "Franco",
-			},
-			To: &mailjet.RecipientsV31{
-				mailjet.RecipientV31{
-					Email: "ferra.main@gmail.com",
-					Name:  "Franco",
-				},
-			},
-			Subject:  "Daily - " + time.Now().Format("06/01/02"),
-			TextPart: "Que tengas buen día wachín!",
-			HTMLPart: html,
-			CustomID: "Daily",
-		},
-	}
-	messages := mailjet.MessagesV31{Info: messagesInfo}
-
-	res, err := mailjetClient.SendMailV31(&messages)
-	if err != nil {
-		log.Println(err.Error())
-	}
-
-	fmt.Printf("Data: %+v\n", res)
+	connect.SendMail("ferra.main@gmail.com", subject, "", html)
 }
 
 func sendWeeklyDoneMail() {
-	//todo config
-	mailjetClient := mailjet.NewMailjetClient("82a01557701a0dd9f319df7c84418785", "151f081f1debe5f691ac6b0cc6caa8eb")
-
 	task := &Task{}
 	weekAgo := time.Now().Add(-24 * 7 * time.Hour)
 	doneAndArchived, _ := task.GetDoneAndArchivedSince(weekAgo)
@@ -149,61 +100,31 @@ func sendWeeklyDoneMail() {
 	doneElements := ""
 	archivedElements := ""
 
-	for i, task := range doneAndArchived {
-		color := "#e4e4e4"
-		if i%2 == 0 {
-			color = "#c3c3c3"
+	for _, task := range doneAndArchived {
+		if task.Status == Done {
+			doneElements += createMailRow(task.Name, "gray", "black", false)
 		}
 
 		if task.Status == Done {
-			doneElements += `<p style="background-color: ` + color + `; padding: 8px; margin: 0;">` + task.Name + `</p>`
-		}
-
-		if task.Status == Done {
-			archivedElements += `<p style="background-color: ` + color + `; padding: 8px; margin: 0;">` + task.Name + `</p>`
+			archivedElements += createMailRow(task.Name, "gray", "black", false)
 		}
 	}
 
+	//send mail
+	subject := "Weekly - " + time.Now().Weekday() + " " + time.Now().Format("06/01/02")
+
 	html := `
 	<html>
-		<head>
-	  		<title>Weekly - ` + weekAgo.Format("06/01/02") + ` - ` + time.Now().Format("06/01/02") + `</title>
-		</head>
-		<body> 
-			<p style="color: white; background-color: black; margin: 0; font-size: 14px; text-align: center; font-weight: bold;">DONE</p>` +
-		doneElements + `
-		<p style="color: white; background-color: black; margin: 0; font-size: 14px; text-align: center; font-weight: bold;">ARCHIVED</p>` +
-		archivedElements + `
-			<p style="background-color: black; margin: 0; font-size: 8px">~</p>
-			<br>
+		<body> ` +
+
+		createMailRow("DONE", "black", "white", true) + doneElements +
+		createMailRow("ARCHIVED", "black", "white", true) + archivedElements + `
+
+		<p style="background-color: black; margin: 0; font-size: 8px">~</p>
+		<br>
 		</body>
 	</html>
 	`
 
-	messagesInfo := []mailjet.InfoMessagesV31{
-		mailjet.InfoMessagesV31{
-			From: &mailjet.RecipientV31{
-				Email: "lucfran2005@hotmail.com",
-				Name:  "Franco",
-			},
-			To: &mailjet.RecipientsV31{
-				mailjet.RecipientV31{
-					Email: "ferra.main@gmail.com",
-					Name:  "Franco",
-				},
-			},
-			Subject:  `Weekly - ` + weekAgo.Format("06/01/02") + ` - ` + time.Now().Format("06/01/02"),
-			TextPart: "Que tengas buena semana wachín!",
-			HTMLPart: html,
-			CustomID: "Weekly",
-		},
-	}
-	messages := mailjet.MessagesV31{Info: messagesInfo}
-
-	res, err := mailjetClient.SendMailV31(&messages)
-	if err != nil {
-		log.Println(err.Error())
-	}
-
-	fmt.Printf("Data: %+v\n", res)
+	connect.SendMail("ferra.main@gmail.com", subject, "", html)
 }
